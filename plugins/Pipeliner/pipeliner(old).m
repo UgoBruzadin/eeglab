da4e07e14cef823e8846c627f370b91b502c6e2f
INLgithub <53059498+INLgithub@users.noreply.github.com>
2020-02-03 23:51:18 -0600

    updated to pipeliner

--- plugins/Pipeliner/pipeliner(old).m
+++ plugins/Pipeliner/pipeliner(old).m
@@ -56,22 +56,29 @@ classdef pipeliner
                 else
                     [EEG, acronym] = action(EEG); %this is where the function runs the asked code!
                 end
+                
                 EEG = eeg_checkset(EEG);
                 [individualReport] = pipeliner.tempReport(files(i).name,EEG);%changedneedsfixing
                 %saving files & report
                 EEG = pop_saveset(EEG, 'filename', [strcat(files(i).name(1:end-4), [acronym], '.set')], 'filepath',filePOST);
                 writetable(cell2table(individualReport),strcat(files(i).name(1:end-4),'_report.xlsx')); %saves the table in .mat format
                 pipeliner.fft(files(i),EEG);
-                %pipeliner.componentFigures(files(i),EEG) %fixed and added 2/3/2020
-                EEG = pop_delset(EEG,1); %fixed and added 2/3/2020
+                %pipeliner.componentFigures(files(i),EEG)
             end
-            %makes report
-            pipeliner.report(folderNameDate); %fixed and added 2/3/2020
+            reports = dir('*.xlsx');
+            for i=1:length(reports)
+                if i==1
+                    finalReport = readtable(reports(i).name);
+                else
+                    finalReport = cat(1,finalReport,readtable(reports(1).name));
+                end
+            end
+            writetable(finalReport, strcat(folderNameDate,'full_report.xlsx'));
             %------
             %cleaning the folder from binica trash
             pipeliner.emptyTrash(); %deletes binica's leftover trash
             %sends text to me
-            pipeliner.txt(strcat('processing of ', folderNameDate,' is over')); %fixed and added 2/3/2020
+            %pipeliner.txt(strcat('processing of is over'));
         end
         
         %functions without EEG
@@ -87,18 +94,6 @@ classdef pipeliner
         %                 end
         %             end
         %         end
-        
-        function report(folderNameDate)
-            reports = dir('*.xlsx');
-            for i=1:length(reports)
-                if i==1
-                    finalReport = readtable(reports(i).name);
-                else
-                    finalReport = cat(1,finalReport,readtable(reports(1).name));
-                end
-            end
-            writetable(finalReport, strcat(folderNameDate,'full_report.xlsx'));
-        end
         
         function emptyTrash()
             trash = dir('bin*');
@@ -126,12 +121,12 @@ classdef pipeliner
             %------ create the folders where the pipeline will run
             %------ filePRE = strcat(basefolder,'\', type, '\pre'); %copies files
             %------ one can I turn it off)
-            cd(batchPath) % - goes to batches's folder
-            mkdir (folderName); % creates new folder
-            mkdir (folderName,  'pre'); %created nwe pre folder inside
-            cd(filePath); %goes to last file location
-            fdtfiles = dir('*.fdt'); %gets fdt files
-            files = dir('*.set'); %gets set files
+            cd(batchPath)
+            mkdir (folderName);
+            mkdir (folderName,  'pre');
+            cd(filePath);
+            fdtfiles = dir('*.fdt');
+            files = dir('*.set');
             %------
             filePRE = strcat(batchPath,'\', folderName, '\pre');
             filePOST = strcat(batchPath,'\',folderName);
@@ -235,44 +230,40 @@ classdef pipeliner
         
         function [EEG, acronym] = icloop(cut, EEG)
             %------ pipeliner.icloop([.8],EEG) or pipeliner.icloop(.8,EEG)
-            %if size(EEG.icaweights,1) == 0
-            %    EEG = pop_runica(EEG, 'extended',1,'interrupt','on','pca',floor(sqrt(EEG.pnts/20)),'verbose','off');
-            %end
+            if ~size(EEG.icaweights,1)
+                EEG = pop_runica(EEG, 'extended',1,'interrupt','on','pca',floor(sqrt(EEG.pnts/20)),'verbose','off');
+            end
             IC = size(EEG.icaweights,1);
-            %oldEEG = EEG;
+            oldEEG = EEG;
             %IC2 = size(EEG.icaweights,1);
             loop = 100;
             for j=1:loop
                 %------ run IC label & flag 
                 EEG = pop_iclabel(EEG, 'default');
                 EEG = pop_icflag(EEG, [NaN NaN;0.97 1;0.97 1;0.97 1;0.97 1;0.97 1;NaN NaN]);
-                
                 %------ store components and update component number
                 mybadcomps = find(EEG.reject.gcompreject);   %stores the Id of the components to be rejected
                 IC = IC - length(mybadcomps);            %stores the number to be the next components analysis
-                %------- reject flagged components
                 EEG = pop_subcomp(EEG, mybadcomps, 0);       % actually removes the flagged components
-                %------- PCA reduction & break if limit reached
-                if size(EEG.icaweights,1) <= table2array(cut(2)) %stops the loop when the number given in the command line
-                    break
-                else
-                    IC = IC - 1;                    % makes the next PCA of 1 number smaller.
-                end
-                %------- run new relative PCA
-                try EEG = pop_runica(EEG,'extended',1,'pca',IC,'verbose','off'); %does a PCA of IC
-                catch EEG = pop_runica(EEG,'extended',1,'pca',IC,'verbose','off'); %BUG: it looks likes it picks up the wrong binica files. inconsistent error.
-                end
-                    
+                
                 %------ reset IC2, which is the last highest component #
                 if ~isempty(mybadcomps)              %if any components are removed, the IC2 stores the last time a pca removed a comp.
                     %IC2 = IC;               %reset the original IC number so we can go back in case no components are removed later on.
                     oldEEG = EEG;
                 end
+                
+                %------- PCA reduction & break if limit reached
+                
+                    IC = IC - 1;                    % makes the next PCA of 1 number smaller.
+                %------- run new relative PCA
+                try EEG = pop_runica(EEG,'extended',1,'pca',IC,'verbose','off'); %does a PCA of IC
+                catch EEG = pop_runica(EEG,'extended',1,'pca',IC,'verbose','off'); %BUG: it looks likes it picks up the wrong binica files. inconsistent error.
+                end
+                if size(EEG.icaweights,1) <= table2array(cut(2)) %stops the loop when the number given in the command line
+                    break
+                end
             end
             %------- return to last highest component, store acronym
-            if ~oldEEG
-                oldEEG = EEG;
-            end
             EEG = oldEEG;
             %EEG = pop_runica(EEG, 'extended',1,'pca',IC2-1,'verbose','off'); %runs a PCA to the last num of ICs since a component was removed
             acronym = char(strcat('IL',num2str(size(EEG.icaweights,1)))); %the acronym to be passed along to be added to the name of the file
@@ -453,23 +444,9 @@ classdef pipeliner
         end
         
         function [EEG, acronym] = epoch(content, EEG) %content must be 1.array of names of epochs, 2. first cut and 3. second cut, both in seconds
-            content = table2array(content);
             %EEG = pop_epoch( EEG, {content(1)},'limits', [content(2) content(3)], 'newname', 'Neuroscan EEG data epochs', 'epochinfo', 'yes');
-            EEG = pop_epoch( EEG, { table2array(content(1)) }, [table2array(content(2)) table2array(content(3))], 'newname', 'Neuroscan EEG data epochs', 'epochinfo', 'yes');
-            fprintf('epoching the data');
-            %EEG = pop_epoch( EEG, { 'DIN' }, [0.400 2.448], 'newname', 'Neuroscan EEG data epochs', 'epochinfo', 'yes');
+            EEG = pop_epoch( EEG, { string(content(1)) }, [content(2) content(3)], 'newname', 'Neuroscan EEG data epochs', 'epochinfo', 'yes');
             
-            %EEG = eeg_regepochs(EEG,content,4.094,'limits',[0 4.094]); %this is will give you 2.044 epoch lenghts as matlab lose the 1st point like n-scan
-            acronym = 'EP';%make for variable epoc name!
-        end
-        
-        function [EEG, acronym] = epochRejection(content, EEG) %content must be 1.array of names of epochs, 2. first cut and 3. second cut, both in seconds
-            %EEG = pop_epoch( EEG, {content(1)},'limits', [content(2) content(3)], 'newname', 'Neuroscan EEG data epochs', 'epochinfo', 'yes');
-            if length(EEG.trials) > 1
-                EEG = pop_jointprob(EEG,1,[1:129] , table2array(content(1)) , table2array(content(2)) , 1 , 1 , 0 , []);
-            end
-            close all
-            fprintf('epoching the data');
             %EEG = pop_epoch( EEG, { 'DIN' }, [0.400 2.448], 'newname', 'Neuroscan EEG data epochs', 'epochinfo', 'yes');
             
             %EEG = eeg_regepochs(EEG,content,4.094,'limits',[0 4.094]); %this is will give you 2.044 epoch lenghts as matlab lose the 1st point like n-scan
@@ -480,20 +457,15 @@ classdef pipeliner
             EEG = pop_rmbase( EEG, [commands(1) commands(2)]);
             acronym = 'BL'; %make for variable baseline later
         end
-        
         %figuregenerators!
-        function fft(files,EEG) %works; by ugo 2/3/2020 7:15pm
-            figure; pop_spectopo(EEG, 1, [EEG.xmin*10^3  EEG.xmax*10^3], 'EEG' , 'freq', [4 5 6 7 8 9 10 11 12 18 30 40], 'freqrange',[2 55], 'electrodes','off');
+        function fft(files,EEG)
+            figure; pop_spectopo(EEG, 1, [EEG.xmin*10^3  EEG.xmax*10^3], 'EEG' , 'freq', [4 6 8 10 12 14 16 18 20 25 30 35 40], 'freqrange',[2 55], 'electrodes','off');
             saveas(gcf,[strcat(files.name(1:end-4),'_FFT.jpg')]);
             close all;
         end
         
-        function componentFigures(files,EEG) %maybe fixed by ugo 2/3/2020 7:15pm
-            IC = size(EEG.icaweights,1);
-            if size(EEG.icaweights,1) > 20
-                IC = 20;
-            end
-            pop_viewprops( EEG, 0,[1:IC], {'freqrange', [2 55]}, {}, 1, 'ICLabel' );
+        function componentFigures(files,EEG)
+            figure; pop_viewprops( EEG, 0,[1:[size(EEG.icaweights,1)]], {'freqrange', [2 55]}, {}, 2, 'ICLabel' )
             saveas(gcf,[strcat(files.name(1:end-4),'_ICS.jpg')]);
             close all;
         end
