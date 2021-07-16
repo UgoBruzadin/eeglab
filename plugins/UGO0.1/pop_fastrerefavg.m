9e624aca1258c50568a08d59387cf9ba31e508f0
Ugo Bruzadin Nunes <ugob@siu.edu>
2021-07-16 15:41:34 -0500

    Deleting QuickLab

--- plugins/UGO0.1/pop_fastrerefavg.m
+++
@@ -1,8 +0,0 @@
-function EEG = pop_fastrerefavg(EEG)
-if isempty(EEG.data)
-    EEG = pop_loadset();
-    eeglab redraw
-end
-EEG = pop_reref( EEG, [],'keepref','on');
-
-end
--- plugins/UGO0.1/quick_rerefavg.m
+++
@@ -1,8 +0,0 @@
-function EEG = quick_rerefavg(EEG)
-if isempty(EEG.data)
-    EEG = pop_loadset();
-    eeglab redraw
-end
-EEG = pop_reref( EEG, [],'keepref','on');
-
-end
