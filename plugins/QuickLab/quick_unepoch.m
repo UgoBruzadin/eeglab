be9c12cebe17a2e36709828a1e1bbd80df83481f
Ugo Bruzadin Nunes <ugob@siu.edu>
2021-07-28 17:58:44 -0500

    Quick unepoch fix. Final fix! It works!
    Needed to change EEG.pnts as well!

--- plugins/QuickLab/quick_unepoch.m
+++ plugins/QuickLab/quick_unepoch.m
@@ -1,18 +1,23 @@
-function EEG = quick_unepoch(EEG)
+function EEG1 = quick_unepoch(EEG)
 
-EEG = pop_selectevent( EEG, 'omittype',{'X'},'deleteevents','on','deleteepochs','off','invertepochs','off');
+EEG1 = EEG;
 
-if EEG.trials > 1
+EEG1 = pop_selectevent( EEG1, 'omittype',{'X'},'deleteevents','on','deleteepochs','off','invertepochs','off');
 
-EEG.data = EEG.data(:,:);
-EEG.xmax = EEG.trials*EEG.pnts;
+if EEG1.trials > 1
+
+EEG1.data = EEG.data(:,:);
+EEG1.xmax = EEG.trials*EEG.pnts;
 if ~isempty(EEG.icaact)
-    EEG.icaact = EEG.icaact(:,:);
+    EEG1.icaact = EEG.icaact(:,:);
     %EEG.icaact = reshape(EEG.icaact , EEG.nbchan, EEG.trials*EEG.pnts);
 end
+EEG1.pnts = EEG1.xmax;
+EEG1.epoch = [];
+EEG1.urevent = EEG.event;
+EEG1.times = 1:EEG.trials*EEG.pnts;
+EEG1.trials = 1;
 
-EEG.times = 1:EEG.trials*EEG.pnts;
-EEG.trials = 1;
 else
     fprintf('Nothing done. File is already in continuous shape \r');
 end
