ac367bfd7503a765f5c1281d3e34b98da43a92b7
Ugo Bruzadin Nunes <ugob@siu.edu>
2021-07-30 17:47:24 -0500

    Fixed cudaica
    removed duplicate command to run

--- plugins/QuickLab/pop_par_runica.m
+++ plugins/QuickLab/pop_par_runica.m
@@ -472,8 +472,7 @@ switch lower(g.icatype)
             disp(['Data rank (' int2str(tmprank) ') is smaller than the number of channels (' int2str(size(tmpdata,1)) ').']);
             [EEG.icaweights,EEG.icasphere] = cudaica(tmpdata, 'lrate', 0.001, 'pca', tmprank, g.options{:}); % Added EEG by Ugo Nunes 06/21/2020
         end
-        
-        [EEG.icaweights,EEG.icasphere] = cudaica(tmpdata, 'lrate', 0.001, g.options{:} );    % Added EEG by Ugo Nunes 06/21/2020 
+        %[EEG.icaweights,EEG.icasphere] = cudaica(tmpdata, 'lrate', 0.001, g.options{:} );    % Added EEG by Ugo Nunes 06/21/2020 
         toc
 %         if isfield(EEG,'dipfit')
 %             if isfield(EEG.dipfit,'model')
