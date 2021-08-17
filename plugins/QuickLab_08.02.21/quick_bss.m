63de9b9cf534bf984933320ed915865e9c58965b
Ugo Bruzadin Nunes <ugob@siu.edu>
2021-08-17 16:38:25 -0500

    bss

--- plugins/QuickLab_08.02.21/quick_bss.m
+++ plugins/QuickLab_08.02.21/quick_bss.m
@@ -1,5 +1,5 @@
-function [EEG,com] = quick_bss(EEG)
+function [EEG] = quick_bss(EEG)
 
-[EEG,com] = pop_autobssemg( EEG, [], [], 'bsscca', {'eigratio', [1000000]}, 'emg_psd', {'ratio', [10],'fs', [250],'femg', [15],'estimator',spectrum.welch,'range', [0  49]});
+EEG = pop_autobssemg( EEG, [], [], 'bsscca', {'eigratio', [1000000]}, 'emg_psd', {'ratio', [10],'fs', [250],'femg', [15],'estimator',spectrum.welch,'range', [0  49]});
 
 end
