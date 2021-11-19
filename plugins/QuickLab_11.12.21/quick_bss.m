8046a06f3a817a201d195670e63d401355a091e4
Ugo Bruzadin Nunes <ugob@siu.edu>
2021-11-19 17:20:57 -0600

    Update to BSS
    BSS now correctly runs at MAX frames, as explained by MATT. It runs on a window of EEG.nbchans/2, if at least 2 epoch-windows.

--- plugins/QuickLab_11.12.21/quick_bss.m
+++ plugins/QuickLab_11.12.21/quick_bss.m
@@ -1,13 +1,13 @@
 function [EEGOUT,com] = quick_bss(EEGIN,window,windowshift)
 
 if nargin < 2
-    window = EEGIN.trials*EEGIN.pnts/EEGIN.srate;
+    window = (EEGIN.trials*EEGIN.pnts/EEGIN.srate)*2;
 end
 if nargin < 3
     windowshift = EEGIN.pnts;
 end
 
-[EEGOUT,com] = pop_autobssemg( EEGIN, [window], [windowshift], 'bsscca', {'eigratio', [1000000]}, 'emg_psd', {'ratio', [10],'fs', [250],'femg', [15],'estimator',spectrum.welch,'range', [0  49]});
+[EEGOUT,com] = pop_autobssemg( EEGIN, [window], [windowshift], 'bsscca', {'eigratio', [1000000]}, 'emg_psd', {'ratio', [10],'fs', [250],'femg', [15],'estimator',spectrum.welch,'range', [0  floor(EEGIN.nbchan/2)]});
 
 plotDifference(EEGIN,EEGOUT)
 
