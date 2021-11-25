5ea8c9f55504b7b49bea4debd9bfbcd33d5377f3
Ugo Bruzadin Nunes <ugob@siu.edu>
2021-11-24 18:56:16 -0600

    Update quick_dipfit.m
    Fixing a stupid bug

--- plugins/QuickLab_11.12.21/quick_dipfit.m
+++ plugins/QuickLab_11.12.21/quick_dipfit.m
@@ -1,12 +1,9 @@
-<<<<<<< Updated upstream
-function EEG = quick_dipfit(EEG,comps)
-=======
+
 function EEG = quick_dipfit(EEG,comps,dipoles)
 
 if nargin < 3
     dipoles = 2; 
 end
->>>>>>> Stashed changes
 
 if nargin < 2
     comps = []; 
@@ -17,6 +14,6 @@ end
 % EEG = pop_par_multifit(EEG, [1:size(EEG.icawinv,2)] ,'threshold',100,'plotopt',{'normlen' 'on'});
 % acronym  = 'DF'; 
 EEG = pop_par_dipfit_settings( EEG, 'hdmfile','C:\\GitHub\\eeglab\\plugins\\dipfit3.7\\standard_BEM\\standard_vol.mat','coordformat','MNI','mrifile','C:\\GitHub\\eeglab\\plugins\\dipfit3.7\\standard_BEM\\standard_mri.mat','chanfile','C:\\GitHub\\eeglab\\plugins\\dipfit3.7\\standard_BEM\\elec\\standard_1005.elc','coord_transform',[0.05476 -17.3653 -8.1318 0.075502 0.0031836 -1.5696 11.7138 12.7933 12.213] ,'chansel',[1:EEG.nbchan] );
-EEG = pop_par_multifit2(EEG, comps ,'threshold',100,'plotopt',{'normlen','on'});
+EEG = pop_par_multifit2(EEG, comps ,'threshold',100,'plotopt',{'normlen','on'},'dipoles',dipoles);
 
 end
