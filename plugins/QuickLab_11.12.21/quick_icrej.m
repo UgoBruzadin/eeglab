dbb0cfb7a44dcaccb0686af087cab8418e6fd76b
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-02-17 14:15:31 -0600

    Update quick_icrej.m
    Fixed it to match new structure; removed deprecated function pop_fastN1PCA.

--- plugins/QuickLab_11.12.21/quick_icrej.m
+++ plugins/QuickLab_11.12.21/quick_icrej.m
@@ -1,4 +1,5 @@
-function [EEG] = quick_icrej(EEG, percent)
+function [EEG,com] = quick_icrej(EEG, percent)
+com = '';
 if nargin < 2
     percent = 0.8;
 else
@@ -11,11 +12,11 @@ if isempty(EEG.icaact)
     EEG.icaact = (EEG.icaweights*EEG.icasphere)*EEG.data(EEG.icachansind,:);
 end
 %if isempty(EEG.etc.ic_classification.ICLabel.classifications)
-EEG = pop_par_iclabel(EEG, 'default');
+[EEG] = pop_par_iclabel(EEG, 'default');
 %end
 IC = size(EEG.icaweights,1);
 
-EEG = pop_par_icflag(EEG, [NaN NaN;percent 1;percent 1;percent 1;NaN NaN;NaN NaN;NaN NaN]);
+[EEG] = pop_par_icflag(EEG, [NaN NaN;percent 1;percent 1;percent 1;NaN NaN;NaN NaN;NaN NaN]);
 
 %                          'Brain''Muscle''Eye''Heart''Line Noise''Channel Noise''Other' };
 %EEG = pop_par_icflag(EEG, [NaN NaN;flag 1;flag 1;flag 1;flag 1;flag 1;NaN NaN]);
@@ -24,7 +25,6 @@ mybadcomps = find(EEG.reject.gcompreject);   %stores the Id of the components to
 %EEG = pop_par_iclabel(EEG, 'default');
  if mybadcomps
      EEG = pop_par_subcomp(EEG, mybadcomps, 0);       % actually removes the flagged components
-     EEG = pop_fastN1PCA(EEG);
      acronym = char(strcat('PcRj',num2str(size(mybadcomps,1)))); %the acronym to be passed along to be added to the name of the file
  else
      acronym = 'PcRj0';
