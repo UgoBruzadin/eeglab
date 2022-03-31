fb45dbf6161c084285ffc621ab2876de3552e751
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-03-31 11:59:38 -0500

    small bug fix

--- plugins/QuickLab_11.12.21/eeg_eegrej2.m
+++ plugins/QuickLab_11.12.21/eeg_eegrej2.m
@@ -314,7 +314,7 @@ end
 
 % Run data rejection in case of rejection selected.
 if ~isempty(regions_for_rej)
-    if size(EEG.data) > 2
+    if size(EEG.data,3) > 1
         rejected_epochs = [];
         for i=1:size(regions_for_rej,1)
             rejected_epochs = [rejected_epochs, floor(regions_for_rej(i,1)/EEG.pnts)+1];
