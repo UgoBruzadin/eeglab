cd23a29c8a388cad37b1ac96cfcb068b3e1bb33f
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-03-31 12:00:56 -0500

    Update pop_par_runica.m

--- plugins/QuickLab_11.12.21/pop_par_runica.m
+++ plugins/QuickLab_11.12.21/pop_par_runica.m
@@ -616,10 +616,24 @@ function tmprank2 = getrank(tmpdata)
     covarianceMatrix = cov(tmpdata', 1);
     [~, D] = eig (covarianceMatrix);
     rankTolerance = 1e-7;
-    tmprank2=sum (diag (D) > rankTolerance);
+    tmprank2 = sum (diag (D) > rankTolerance);
     if tmprank ~= tmprank2
         fprintf('Warning: fixing rank computation inconsistency (%d vs %d) most likely because running under Linux 64-bit Matlab\n', tmprank, tmprank2);
         tmprank2 = min(tmprank, tmprank2);
     end
-            
+           
+function tmprank2 = getrankMax(tmpdata)
+    
+    tmprank = rank(tmpdata);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Here: alternate computation of the rank by Sven Hoffman
+    %tmprank = rank(tmpdata(:,1:min(3000, size(tmpdata,2)))); old code
+    covarianceMatrix = cov(tmpdata', 1);
+    [~, D] = eig (covarianceMatrix);
+    rankTolerance = 1e-7;
+    tmprank2 = sum (diag (D) > rankTolerance);
+    if tmprank ~= tmprank2
+        fprintf('Warning: fixing rank computation inconsistency (%d vs %d) most likely because running under Linux 64-bit Matlab\n', tmprank, tmprank2);
+        tmprank2 = max(tmprank, tmprank2);
+    end
             
