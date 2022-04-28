08a4784012108d179676448453b9001a026348c3
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-04-28 12:15:26 -0500

    Update quick_spectra.m
    Made the code more generic and dependent upon the defaults file

--- plugins/QuickLab_11.12.21/quick_spectra.m
+++ plugins/QuickLab_11.12.21/quick_spectra.m
@@ -1,51 +1,76 @@
-function [EEG,com] = quick_spectra(EEG,high,low,avg)
-if isempty(EEG.data)
-    [EEG,com] = pop_loadset();
-    [EEG,com] = eeg_store(EEG);
+% [com] = quick_spectra(EEG, high, low, reference) - Quickly plots channel
+%                                                    frequency spectra
+%
+% Author: Ugo Bruzadin Nunes
+%
+% Copyright (C) 2021 Ugo Bruzadin Nunes
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+function [com] = quick_spectra(EEGIN,high,low,topo,references)
+
+if isempty(EEGIN.data)
+    [EEGIN,com] = pop_loadset();
+    [EEGIN,com] = eeg_store(EEGIN);
 end
 
-QuickLabDefs;
-try
-    topo = SPECTRATOPO;
-    high = SPECTRADEFS(1);
-    low = SPECTRADEFS(2);
-    maxWindow = 2^floor(log2(EEG.pnts));
-    if maxWindow > SPECTRADEFS(3)
-        maxWindow = SPECTRADEFS(3);
-    end
-catch
-maxWindow = 2^floor(log2(EEG.pnts));
-if maxWindow > 2048
-    maxWindow = 2048;
+% collects defaults defined at QuickLabDefs
+
+QuickLabDefs; % using SPECTRADEFS 1 for High Frequency filter, 2 for low frequency filter and 3 for maximum points for FFT display
+              % also using SPECTRATOPO for defaulty defined frequencies to display
+%% collecting defaults or given variables
+              
+if nargin > 3
+    [EEGIN,com] = quick_reref(EEGIN,references);
 end
+
 if nargin < 4
-    avg = 0;
-end
-if avg
-    [EEG,com] = pop_fastrerefavg(EEG);
+   topo = SPECTRATOPO; % SPECTRATOPO DEFINED INSIDE QuickLabDefs 
 end
 
 if nargin < 3 
-    low = 2;
+    %low = 2;
+    low = SPECTRADEFS(2); % SPECTRADEFS DEFINED INSIDE QuickLabDefs 
 end
 
 if nargin < 2
-    high = 55;
+    %high = 55;
+    high = SPECTRADEFS(1); % SPECTRADEFS DEFINED INSIDE QuickLabDefs 
+    
+maxWindow = 2^floor(log2(EEGIN.pnts));
+if maxWindow > SPECTRADEFS(3)
+    %maxWindow = 2048;
+    maxWindow = SPECTRADEFS(3);
 end
+ 
+% numberOfHeadmaps = 10;
+% 
+% calc = (high - low) / numberOfHeadmaps;
+% topo = zeros(1,numberOfHeadmaps);
+% 
+% for i=1:numberOfHeadmaps
+%     topo(i) = floor(low + i*calc);
+% end
 
-numberOfHeadmaps = 10;
+%topo = [4 5 6 7 8 9 10 11 12 15 20 25 30 36];
 
-calc = (high - low) / numberOfHeadmaps;
-topo = zeros(1,numberOfHeadmaps);
+%% runs pop_spectopo with the given defaults or variables
 
-for i=1:numberOfHeadmaps
-    topo(i) = floor(low + i*calc);
-end
-
-topo = [4 5 6 7 8 9 10 11 12 15 20 25 30 36];
-end
 tic
-figure; [EEG.x,EEG.y] = pop_spectopo(EEG, 1, [EEG.xmin*1000  EEG.xmax*1000], 'EEG' , 'freq', [topo], 'freqrange',[low high],'winsize',maxWindow,'electrodes','off');
+figure; pop_spectopo(EEGIN, 1, [EEGIN.xmin*1000  EEGIN.xmax*1000], 'EEG' , 'freq', [topo], 'freqrange',[low high],'winsize',maxWindow,'electrodes','off');
 toc
 
 end
