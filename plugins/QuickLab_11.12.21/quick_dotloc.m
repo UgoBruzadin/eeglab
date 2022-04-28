9bc98ec6bae700315e841d22014f48fd71500457
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-04-28 12:17:29 -0500

    Create quick_dotloc.m
    Added this simple function to run a 92 Headmodel, Epoch data and BSS all at once to save time

---
+++ plugins/QuickLab_11.12.21/quick_dotloc.m
@@ -0,0 +1,11 @@
+function [EEG,com] = quick_dotloc(EEG)
+
+[EEG,com] = quick_HM94(EEG);
+
+[EEG,com] = quick_epoch(EEG,0.600,2.648);
+
+[EEG,com] = quick_bss2(EEG);
+
+[EEG] = pop_saveset(EEG, 'filename', [strcat( EEG.filename(1:end-4),'Hm92Ep6bss','.set')],'filepath',EEG.filepath);
+
+eeglab redraw
