d8d353fffebb07e7197b9ccbce701548ffcdc508
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-05-27 16:36:25 -0500

    Update plotfft.m
    Added pie chart, changes the plotting function!

--- plugins/QuickLab_11.12.21/plotfft.m
+++ plugins/QuickLab_11.12.21/plotfft.m
@@ -20,15 +20,22 @@
 %  %imshow(J);
 
 
-function rec = plotfft(EEG,files,findex)
+function rec = plotfft(EEG,files,jpgindex)
 
 
 %jpgfiles = dir('*jpg');
 % if isempty(findex)
 %     findex = find(strcmp({files.name}, strcat(EEG.filename)));
 % end
+W_MAIN = findobj('Tag','EEGLAB');
 
+axisPie = uipanel(W_MAIN, 'Position',[.9 .05 .05 .25],'Tag','PieChart',Visible='on',BackgroundColor=W_MAIN.Color,BorderType='none');
+axisFFT = uipanel(W_MAIN, 'Position',[.5 .15 .35 .67],'Tag','Picture',Visible='on',BackgroundColor=W_MAIN.Color,BorderType='none');
+% % Create a new axis on the panel
+pieAxis = axes(axisPie, 'Tag','pie_chart','Position', [0 0 1 1],Visible='on');
+fftAxis = axes(axisFFT, 'Tag','fft_chart','Position', [0 0 1 1],Visible='on');
 
+CountDownDotLoc(pieAxis);
 
 if exist('EEG','var')
     if isstruct(EEG)
@@ -38,18 +45,58 @@ if exist('EEG','var')
             end
             rec = [];
             jpgfiles = dir('*FFT*jpg');
-            findex = find(strcmp({jpgfiles.name}, strcat(EEG.filename(1:end-4),'FFT.jpg')));
-            if ~isempty(findex)
-                rec = uicontrol(gcf, 'Style', 'pushbutton', 'Units','Normalized','Tag', 'rec', 'Position', [.5 .15 .35 .67]);
-                pic = jpgfiles(findex).name;
-                [picrgb,map] = imread(pic);
-                set(rec,'Units','pixels')
-                resizePos = get(rec,'Position');
-                [picrgb2,map] = imresize(picrgb, [resizePos(4) resizePos(3)]);
-                set(rec,'Units','normalized');
-                set(rec,'cdata',picrgb2);
+            jpgindex = find(strcmp({jpgfiles.name}, strcat(EEG.filename(1:end-4),'FFT.jpg')));
+            figfiles = dir('*FFT*fig');
+            figindex = find(strcmp({figfiles.name}, strcat(EEG.filename(1:end-4),'FFT.fig')));
+            if ~isempty(jpgindex)
+%                
+%                 
+                 pic = jpgfiles(jpgindex).name;
+                 rec = imshow(pic,'Parent',fftAxis,'InitialMagnification','fit');
+            elseif ~isempty(figindex)
+% 
+%                  pic = figfiles(figindex).name;
+%                  
+%                  rec = openfig(pic,'new', 'invisible');
+%                  set(rec,'Tag','rec')
+%                  hAxes = get(rec, 'CurrentAxes');
+%                  hCopy = copyobj(hAxes, handles.rec);
+%                  set(hCopy, 'Position', [.5 .15 .35 .67])
+
+
+
+
+
+% %                 rec = uicontrol(gcf, 'Style', 'pushbutton', 'Units','Normalized','Tag', 'rec', 'Position', [.5 .15 .35 .67]);
+%                 %fig = uifigure(gcf);
+%                 [picrgb,map] = imread(pic);
+%                 %uiimage(fig,"ImageSource",pic);
+%                 
+%                 %imshow(rec,pic2,'Parent',W_MAIN)
+%                 set(rec,'Units','pixels')
+%                 resizePos = get(rec,'Position');
+%                 [picrgb2,map] = imresize(picrgb, [resizePos(4) resizePos(3)]);
+%                 set(rec,'Units','normalized');
+%                 set(rec,'cdata',picrgb2);
             end
+            %fig = uifigure(gcf);
+            %uiimage(fig,"ImageSource",pic);
             
+%             resizePos = get(rec,'Position');
+%                  [picrgb2,map] = imresize(picrgb, [resizePos(4) resizePos(3)]);
+%                  set(rec,'Units','normalized');
+%                  set(rec,'cdata',picrgb2);
+            %rec = uipanel(W_MAIN, 'Position', [.5 .15 .35 .67],'Tag','rec')
+
+            %try
+%                 [total,done] = CountDownDotLoc()
+%                 pieC = uicontrol(gcf, 'Style', 'pushbutton', 'Units','Normalized','Tag', 'pie', 'Position', [.9 .05 .05 .05]);
+%                 set(pieC,'Units','pixels')
+%                 resizePos = get(pieC,'Position');
+%                 pieData = pie([total-done,done]);
+%                 set(pieC,'cdata',pieData);
+            %end
+
         end
     end
 end
