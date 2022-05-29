32267c6c87675bc5e40100392fcf6a5a372bd66a
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-05-29 09:16:59 -0500

    Update eeglab.m
    Fixed rank for components

--- eeglab.m
+++ eeglab.m
@@ -2195,7 +2195,7 @@ rankTolerance = 1e-7;
 tmprank2=sum (diag (D) > rankTolerance);
 if tmprank ~= tmprank2
     %fprintf('Warning: fixing rank computation inconsistency (%d vs %d) most likely because running under Linux 64-bit Matlab\n', tmprank, tmprank2);
-    tmprank2 = max(tmprank, tmprank2);
+    tmprank2 = min(tmprank, tmprank2);
 end
 
 
