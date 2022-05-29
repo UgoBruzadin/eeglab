3fd7c0737223959ae489846c625b1dce4af81a81
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-05-29 14:56:42 -0500

    Update QuickLabDefs.m
    Edited topoplots to include more plots!

--- plugins/QuickLab_11.12.21/QuickLabDefs.m
+++ plugins/QuickLab_11.12.21/QuickLabDefs.m
@@ -36,7 +36,7 @@ NOTCHFILTER = 60;
 % Highest Frequency, Lowest Frequency, Maxwindow
 SPECTRADEFS = [40,2,2048];
 % Specific Expected Frequencies for Topoplot Display 
-SPECTRATOPO = [4 5 6 7 8 9 10 11 12 15 20 25 30 36];
+SPECTRATOPO = [4 5 6 7 8 9 10 11 12 15 18 20 25 30 36];
 
 %% Headmodel and references defaults
 % Location of Headmodel(s)
