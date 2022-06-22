3c94cee63881e0b17a942e400021c31b6681c637
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-06-22 12:17:45 -0500

    Update pop_viewprops2_checkBox.m
    Function added for viewprops2

--- plugins/QuickLab_11.12.21/pop_viewprops2_checkBox.m
+++ plugins/QuickLab_11.12.21/pop_viewprops2_checkBox.m
@@ -1,5 +1,4 @@
-function pop_viewprops2_checkBox(index)
-
+function pop_viewprops2_checkbox(index)
 
 % --- click or unclick the tag
 clickVal = get(findobj(gcf,'Tag',int2str(index)),'Value');
@@ -11,8 +10,10 @@ set(findobj(gcf,'Tag',int2str(index)),'Value', abs(clickVal-1));
 clickVal = abs(clickVal-1);
 
 if clickVal == 1
+    %set(findobj(gcf,'Style','checkbox'),'BackgroundColor',[1 .5 .5])
     set(findobj(gcf,'Tag',strcat('comp',int2str(index))),'BackgroundColor',[1 .5 .5])
 else
+    %set(findobj(gcf,'Style','checkbox'),'BackgroundColor',[.75 1 .75])
     set(findobj(gcf,'Tag',strcat('comp',int2str(index))),'BackgroundColor',[.75 1 .75])
 end
 
