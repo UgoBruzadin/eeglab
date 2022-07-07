9a13b24de5b24773c02e7ff4c1658668cf67fa76
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-07-07 13:13:00 -0500

    Update quick_HM94.m
    Cleaned the array

--- plugins/QuickLab_11.12.21/quick_HM94.m
+++ plugins/QuickLab_11.12.21/quick_HM94.m
@@ -4,7 +4,8 @@ if isempty(EEG.data)
     [EEG,com] = pop_loadset();
     eeglab redraw
 end
-[EEG,com] = pop_select( EEG,'nochannel',[1 8	14	17	21	25	32 38 43 44 48	49	56	63	64	68	69	73	74	81	82	88	89	94	95	99	107	113	114 119	120	121 125	126	127	128]);
+
+[EEG,com] = pop_select( EEG,'nochannel',[1 8 14 17 21 25 32 38 43 44 48 49 56 63 64 68 69 73 74 81 82 88 89 94 95 99 107 113 114 119 120 121 125 126 127 128]);
 
 for a=1:EEG.nbchan
     if isempty(strfind(EEG.chanlocs(1,a).labels,'Cz'))
