5ddb4d918d6f44fbb656d96ab79ae0431a55dc2a
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-08-27 18:38:39 -0500

    Update quick_HM94.m
    Added fix to removed all leftover channels instead of crashing when trying to remove channels not present.

--- plugins/QuickLab_11.12.21/quick_HM94.m
+++ plugins/QuickLab_11.12.21/quick_HM94.m
@@ -11,12 +11,16 @@ EEG.icaact = []; EEG.icaweights = []; EEG.icasphere= []; EEG.icachansind = [];
 % if isempty(EEG.icaact)
 %     EEG.icaact = (EEG.icaweights*EEG.icasphere)*EEG.data(EEG.icachansind,:);
 % end
-
-[EEG,com] = pop_select( EEG,'nochannel',[1 8 14 17 21 25 32 38 43 44 48 49 56 63 64 68 69 73 74 81 82 88 89 94 95 99 107 113 114 119 120 121 125 126 127 128]);
-
+chans = [1 8 14 17 21 25 32 38 43 44 48 49 56 63 64 68 69 73 74 81 82 88 89 94 95 99 107 113 114 119 120 121 125 126 127 128];
+try [EEG,com] = pop_select( EEG,'nochannel',[1 8 14 17 21 25 32 38 43 44 48 49 56 63 64 68 69 73 74 81 82 88 89 94 95 99 107 113 114 119 120 121 125 126 127 128]);
+catch
+for i = chans
+    pop_select( EEG,'nochannel',[i])
+end
+end
 for a=1:EEG.nbchan
     if isempty(strfind(EEG.chanlocs(1,a).labels,'Cz'))
-        EEG.chanlocs(1,a).labels = strcat(EEG.chanlocs(1,a).labels,'_','N',num2str(a));
+        try EEG.chanlocs(1,a).labels = strcat(EEG.chanlocs(1,a).labels,'_','N',num2str(a)); catch; end
     end
 end
 
