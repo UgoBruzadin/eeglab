9f5b225d7e0238c11fd1eb6fd638659a3b27c56f
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-08-29 20:00:39 -0500

    Revert "Update quick_reref.m"
    This reverts commit b9140225136e9d6b7d1be5be4ba8b460b1a541fa.

--- plugins/QuickLab_11.12.21/quick_reref.m
+++ plugins/QuickLab_11.12.21/quick_reref.m
@@ -42,19 +42,18 @@ if nargin < 2
     [EEG,com] = pop_reref( EEG, [], 'keepref', keepref);
 else
 if strcmp(channels,'AVG')
-   [EEG,com] = pop_reref( EEG, [], 'refloc',struct('labels',{'Cz'},'Y',{0},'X',{0},'Z',{8.7919},'sph_theta',{0},'sph_phi',{0},'sph_radius',{0},'theta',{0},'radius',{0},'type',{''},'ref',{'Cz'},'urchan',{[]},'datachan',{0}));
-
+   [EEG,com] = pop_reref( EEG, [], 'keepref', keepref);
 elseif strcmp(channels,'LE')
-   [EEG,com] = pop_reref( EEG, LE_REFS, 'refloc',struct('labels',{'Cz'},'Y',{0},'X',{0},'Z',{8.7919},'sph_theta',{0},'sph_phi',{0},'sph_radius',{0},'theta',{0},'radius',{0},'type',{''},'ref',{'Cz'},'urchan',{[]},'datachan',{0}));
+   [EEG,com] = pop_reref( EEG, LE_REFS, 'keepref', keepref);
 elseif strcmp(channels,'OG')
-   [EEG,com] = pop_reref( EEG, OG_REF,'refloc',struct('labels',{'Cz'},'Y',{0},'X',{0},'Z',{8.7919},'sph_theta',{0},'sph_phi',{0},'sph_radius',{0},'theta',{0},'radius',{0},'type',{''},'ref',{'Cz'},'urchan',{[]},'datachan',{0}));
+   [EEG,com] = pop_reref( EEG, OG_REF, 'keepref', keepref);
 elseif strcmp(channels,'CZ')
     options = REFLOC;
    [EEG,com] = pop_reref( EEG, OG_REF, 'keepref', keepref, 'refloc',options);
 else
     %try finding channel names
     %don't reference otherwise?
-    [EEG,com] = pop_reref( EEG, channels, 'keepref', keepref, 'refloc',struct('labels',{'Cz'},'Y',{0},'X',{0},'Z',{8.7919},'sph_theta',{0},'sph_phi',{0},'sph_radius',{0},'theta',{0},'radius',{0},'type',{''},'ref',{'Cz'},'urchan',{[]},'datachan',{0}));
+    [EEG,com] = pop_reref( EEG, channels, 'keepref', keepref);
 end
 
 end
