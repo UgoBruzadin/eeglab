f3cce1fa0d7e2a7df371db9d0b3c18c12ad45822
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-09-08 23:43:12 -0500

    Update eegplot_w3.m
    Fixed issue with make_eloc_file for TBT, not necessary and was redrawing all badchans in eloc-chan

--- plugins/QuickLab_11.12.21/eegplot_w3.m
+++ plugins/QuickLab_11.12.21/eegplot_w3.m
@@ -4368,8 +4368,10 @@ function plot_topoplot_old(fig)
                 g = TBT(g);
             case 2
                 g = QUICKLAB(g);
+                g = make_eloc_file(g);
             case 3
                 g = ICLABEL(g);
+                g = make_eloc_file(g);
         end
         
         %g = THINKING(g,0);
@@ -4378,7 +4380,7 @@ function plot_topoplot_old(fig)
 %         g.old{end+1} = g;
 %         g.gnumber = length(g.old);
 
-        g = make_eloc_file(g);
+        %g = make_eloc_file(g);
         set(gcf,'UserData',g);
         eegplot_w3('setelect');
         eegplot_w3('winelec_auto');
