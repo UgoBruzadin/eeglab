b3e85762dfa4b3106114b36556a574da733f9a96
Ugo Bruzadin Nunes <ugob@siu.edu>
2022-09-30 12:40:36 -0500

    Update plotfft.m

--- plugins/QuickLab_11.12.21/plotfft.m
+++ plugins/QuickLab_11.12.21/plotfft.m
@@ -7,10 +7,10 @@ function rec = plotfft(EEG,files,jpgindex)
 % end
 W_MAIN = findobj('Tag','EEGLAB');
 
-axisPie = uipanel(W_MAIN, 'Position',[.9 .05 .09 .30],'Tag','PieChart',Visible='on',BackgroundColor=W_MAIN.Color,BorderType='none');
+axisPie = uipanel(W_MAIN, 'Position',[.87 .05 .10 .30],'Tag','PieChart',Visible='on',BackgroundColor=W_MAIN.Color,BorderType='none');
 
 % % Create a new axis on the panel
-pieAxis = axes(axisPie, 'Tag','pie_chart','Position', [0 0 1 1],Visible='on');
+pieAxis = axes(axisPie, 'Tag','pie_chart','Position', [0.2 0.2 .6 .6],Visible='on');
 %axis fill
 CountDownDotLoc(pieAxis);
 
