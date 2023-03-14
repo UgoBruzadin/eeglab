41daa662e7d3dcf12213ddefbf612e7657e0a7ba
Ugo Bruzadin Nunes <ugob@siu.edu>
2023-03-14 16:36:55 -0500

    Update eeglab.m
    altered to plot all unfinished files instead of all started files

--- eeglab.m
+++ eeglab.m
@@ -1304,7 +1304,7 @@ set(H_MAIN(1), 'unit', 'normalized');
 % save.
 
 % load directory
-loaddircommand = ['findex = [1];try cd(EEG.filepath); catch, end; filecount = [1];files = dir(''.set'');findex = find(strcmp({files.name}, EEG.filename));files = dir(''*.set'');set(findobj(''tag'',''LoadFileList''),''string'',{files(1:end).name},''value'',find(strcmp({files.name}, EEG.filename)));[notstarted, notfinished] = DotLoc_Left_over_files(); set( findobj( ''tag'',''LoadFileList2'' ),''string'',{notstarted(1:end).name});  rec = plotfft(EEG,files,findex);'];
+loaddircommand = ['findex = [1];try cd(EEG.filepath); catch, end; filecount = [1];files = dir(''.set'');findex = find(strcmp({files.name}, EEG.filename));files = dir(''*.set'');set(findobj(''tag'',''LoadFileList''),''string'',{files(1:end).name},''value'',find(strcmp({files.name}, EEG.filename)));[notstarted, notfinished] = DotLoc_Left_over_files(); set( findobj( ''tag'',''LoadFileList2'' ),''string'',{notfinished(1:end).name});  rec = plotfft(EEG,files,findex);'];
 % save command
 savecommand = ['[EEG] = pop_saveset(EEG, ''filename'', [strcat( EEG.filename(1:end-4),get(findobj(''tag'',''SAVETEXT''),''string''),''.set'')],''filepath'',EEG.filepath);'...
      '[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);' loaddircommand 'eeglab redraw;']; %save set ADDED BY UGO
@@ -1315,7 +1315,7 @@ loadpostcommand = ['findex = find(strcmp({files.name}, EEG.filename));if findex 
 % filelist
 loadfilecommand = ['try cd(EEG.filepath); catch, end;EEG = pop_loadset(files(get(findobj(''tag'',''LoadFileList''),''value'')).name, pwd);eeglab redraw;rec = plotfft(EEG,files,findex);'];
 % filelist
-loadfilecommand2 = ['try cd(EEG.filepath); catch, end;EEG = pop_loadset(notstarted(get(findobj(''tag'',''LoadFileList2''),''value'')).name, pwd);eeglab redraw;rec = plotfft(EEG,files,findex);'];
+loadfilecommand2 = ['try cd(EEG.filepath); catch, end;EEG = pop_loadset(notfinished(get(findobj(''tag'',''LoadFileList2''),''value'')).name, pwd);eeglab redraw;rec = plotfft(EEG,files,findex);'];
 
 
 if ~exist("EEG")
