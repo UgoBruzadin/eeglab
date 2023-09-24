e25bb523d716961221af14052c4109d1e0912526
Ugo Bruzadin Nunes <ugob@siu.edu>
2023-09-23 17:26:02 -0700

    Deleted extra folders
    Maintained only the main code folders!

--- plugins/QuickLab0.1/non-quicklab-functions/GetSpecificFiles.m
+++
@@ -1,5 +0,0 @@
-function filelist = GetSpecificFiles(files,marker)
-
-filelist = files(~cellfun(@isempty,regexp({files.name}, marker)));
-
-end
--- plugins/QuickLab0.1/non-quicklab-functions/pop_fastrerefchannels.m
+++
@@ -1,9 +0,0 @@
-function EEG = pop_fastrerefchannels(EEG,channels)
-if isempty(EEG.data)
-    EEG = pop_loadset();
-    eeglab redraw
-end
-
-EEG = pop_reref( EEG, channels,'keepref','on');
-
-end
